e34e84d3f1f091eca740088e6a8d04a22b0def7f
Klaudiusz-droid <kpiestrzynski@edu.cdv.pl>
2026-07-25 19:31:24 +0200

    add MapData

---
+++ Assets/Scripts/MapGenerate/MapData.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData
+{
+    public int Width { get; private set; }
+    public int Depth { get; private set; }
+    public float BlockSize { get; private set; }
+    
+    public int[,] HeightMap { get; set; }
+    public int[,] RegionMap { get; set; }
+    public bool[,] IsOccupied { get; set; }
+    public Dictionary<int, List<Vector2Int>> RegionTiles { get; set; }
+
+    public MapData(int width, int depth, float blockSize)
+    {
+        Width = width;
+        Depth = depth;
+        BlockSize = blockSize;
+        HeightMap = new int[width, depth];
+        RegionMap = new int[width, depth];
+        IsOccupied = new bool[width, depth];
+        RegionTiles = new Dictionary<int, List<Vector2Int>>();
+    }
+}
